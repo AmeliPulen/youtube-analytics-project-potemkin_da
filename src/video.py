0d62b5ef6d3e5c53e2cd2fc23a9584dadcc27b09
AmeliPulen <139628158+AmeliPulen@users.noreply.github.com>
2023-11-08 00:47:21 +0300

    Added playlist.py, small bug fixes

--- src/video.py
+++ src/video.py
@@ -2,10 +2,11 @@ import os
 
 from googleapiclient.discovery import build
 
-api_key: str = os.getenv('YT_API_KEY')
-youtube = build('youtube', 'v3', developerKey=api_key)
 
 class Video():
+
+    api_key: str = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, video_id: str) -> None:
         """
